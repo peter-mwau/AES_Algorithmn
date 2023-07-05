e858f26a8f0bd2bc60714a6f144b22a767c67aee
Peter Kyale <kyalepeter2000@gmail.com>
2023-07-05 09:12:52 +0300

    Configured the login technicalities and verification

--- aes_algorithm/models.py
+++ aes_algorithm/models.py
@@ -4,7 +4,8 @@ class UserData(models.Model):
     username = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    salt = models.CharField(max_length=100)
     
     def __str__(self):
-        return self.username, self.email, self.password
+        return f'self.username, self.email, self.password, self.salt'
     
--- aes_algorithm/views.py
+++ aes_algorithm/views.py
@@ -1,12 +1,40 @@
-from django.shortcuts import render
-from .forms import Register
-from .models import UserData
 import bcrypt
-
 from django.shortcuts import render
 from django.contrib import messages
 from .models import UserData
-import bcrypt
+
+# ...
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Generate a new salt
+        salt = bcrypt.gensalt()
+
+        # Hash the password with the generated salt
+        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+        # Convert the hashed password and salt to strings
+        hashed_str = hashed.decode('utf-8')
+        salt_str = salt.decode('utf-8')
+
+        # Storing the details to the database
+        user = UserData(username=username, email=email, password=hashed_str, salt=salt_str)
+        user.save()
+
+        context = {
+            'email': email,
+            'username': username,
+            'password': hashed_str
+        }
+
+        return render(request, 'register.html', context)
+
+    return render(request, 'register.html')
+
 
 def index(request):
     if request.method == 'POST':
@@ -16,55 +44,24 @@ def index(request):
         try:
             # Retrieve user data from the database
             user = UserData.objects.get(username=username)
-            hashed_password = user.password
+            hashed_password = user.password.encode('utf-8')
+            salt = user.salt.encode('utf-8')
 
-            # Compare the hashed passwords
-            if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
+            # Concatenate the salt and the hashed password
+            hashed_input_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+            # Compare the concatenated passwords
+            if hashed_password == hashed_input_password:
                 # Redirect to homepage or perform necessary actions
                 return render(request, 'homepage.html')
             else:
                 # Passwords don't match
                 messages.error(request, 'Invalid username or password.')
                 return render(request, 'index.html')
-        
+
         except UserData.DoesNotExist:
             # User data not found
             messages.error(request, 'Invalid username or password.')
             return render(request, 'index.html')
 
     return render(request, 'index.html')
-
-
-def register(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        # Adding the salt to password
-        salt = bcrypt.gensalt()
-         
-        # Hash the password and store all details to the database
-        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-
-        # Printing the salt
-        print("Salt:")
-        print(salt)
- 
-        # Printing the hashed
-        print("Hashed:")
-        print(hashed)
-
-        # Storing the details to the database
-        user = UserData(username=username, email=email, password=hashed)
-        user.save()
-
-        context = {
-            'email': email,
-            'username': username,
-            'password': hashed
-        }
-
-        return render(request, 'register.html', context)
-
-    return render(request, 'register.html')
